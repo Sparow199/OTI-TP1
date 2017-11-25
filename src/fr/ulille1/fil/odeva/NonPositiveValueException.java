ca4c5ef35f1f8af1563575c59a10e3cf94f3b593
Aoudia <mf.aoudia@gmail.com>
2017-11-25 10:28:01 +0100

    Add maven support

--- src/fr/ulille1/fil/odeva/NonPositiveValueException.java
+++
@@ -1,8 +0,0 @@
-package fr.ulille1.fil.odeva;
-
-public class NonPositiveValueException extends Exception {
-    NonPositiveValueException(int value)
-    {
-        super(value+" is negative !!!");
-    }
-}
